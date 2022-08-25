ee8d9ef94ecf816314df7434e91d39a11b9391fa
rodolphe <rodolphe.schnetzer@gmail.com>
2022-08-25 16:29:02 +0200

    change value checkmark

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,19 +9,116 @@ import UIKit
 
 class ChecklistViewControllerViewController: UITableViewController {
     
+    @IBAction func addItem(_ sender: Any) {
+       
+    }
+    
+    let row0text = "Walk the dog"
+    let row1text = "Brush teeth"
+    let row2text = "Learn iOS development"
+    let row3text = "Soccer practice"
+    let row4text = "Eat ice cream"
+    
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.reloadData()
+        
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    
+    // MARK: - TableView data source
+    
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "item", for: indexPath)
-        return cell
-    }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
+        
+        
+        let label = cell.viewWithTag(1000) as! UILabel
     
+        if indexPath.row == 0 {
+            label.text = row0text
+          } else if indexPath.row == 1 {
+            label.text = row1text
+          } else if indexPath.row == 2 {
+            label.text = row2text
+          } else if indexPath.row == 3 {
+            label.text = row3text
+          } else if indexPath.row == 4 {
+            label.text = row4text
+          }
+        configureCheckmark(for: cell, at: indexPath)
+          return cell
+        }
+        
+
+    
+    
+    // MARK: - tableview delegate
+    
+    // deselectionne la cellule touché change le checkmark
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            var isChecked = false
+
+            if indexPath.row == 0 {
+               row0checked.toggle()
+               isChecked = row0checked
+             } else if indexPath.row == 1 {
+               row1checked.toggle()
+               isChecked = row1checked
+             } else if indexPath.row == 2 {
+               row2checked.toggle()
+               isChecked = row2checked
+             } else if indexPath.row == 3 {
+               row3checked.toggle()
+               isChecked = row3checked
+             } else if indexPath.row == 4 {
+               row4checked.toggle()
+               isChecked = row4checked
+             }
+             if isChecked {
+               cell.accessoryType = .checkmark
+             } else {
+               cell.accessoryType = .none
+             }
+           }
+          tableView.deselectRow(at: indexPath, animated: true)
+        }
+    
+    func configureCheckmark(for cell: UITableViewCell, at indexPath: IndexPath) {
+      var isChecked = false
+
+      if indexPath.row == 0 {
+        isChecked = row0checked
+      } else if indexPath.row == 1 {
+        isChecked = row1checked
+      } else if indexPath.row == 2 {
+        isChecked = row2checked
+      } else if indexPath.row == 3 {
+        isChecked = row3checked
+      } else if indexPath.row == 4 {
+        isChecked = row4checked
+      }
+
+      if isChecked {
+        cell.accessoryType = .checkmark
+      } else {
+        cell.accessoryType = .none
+      }
+    }
 }
     
+
+
---
+++ Checklists/DataCheckList.swift
@@ -0,0 +1,26 @@
+//
+//  DataCheckList.swift
+//  Checklists
+//
+//  Created by Rodolphe Schnetzer on 24/08/2022.
+//
+
+import Foundation
+import UIKit
+
+class DataCheckList: UIViewController {
+
+var rowText = ["Walk the dog", "Brush teeth","Learn iOS development","Soccer practice","Eat ice cream"]
+
+//let row0text = "Walk the dog"
+//  let row1text = "Brush teeth"
+//  let row2text = "Learn iOS development"
+//  let row3text = "Soccer practice"
+//  let row4text = "Eat ice cream"
+
+var row0checked = false
+var row1checked = false
+var row2checked = false
+var row3checked = false
+var row4checked = false
+}
